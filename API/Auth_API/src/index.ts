90ab5b1ae6383030be346e18478ace177fd27fcd
JuanCSUCoder <jcsu2019369@gmail.com>
2024-09-22 16:11:42 -0500

    Feat: base aplicación de express

--- API/Auth_API/src/index.ts
+++ API/Auth_API/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express()
+const port = process.env.PORT || 2001;
+
+app.use(express.json());
+
+app.post('/login', (_req, _res) => {
+
+});
+
+app.listen(port, () => {
+    console.log("Escuchando en el puerto " + port + "...");
+});
