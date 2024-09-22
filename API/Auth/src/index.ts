c4706a40c28cccb603668666ce37ccf083422fc2
JuanCSUCoder <jcsu2019369@gmail.com>
2024-09-22 13:42:07 -0500

    Feat: Agrega doc de swagger

--- API/Auth/src/index.ts
+++ API/Auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { expressjwt } from 'express-jwt';
 import { Client } from 'pg';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 
 console.log("Inicializando express...");
@@ -21,15 +23,53 @@ client.connect()
     })
     .catch((e) => {
         console.error("Error connecting to PostgreSQL database", e)
-    })
+    });
 
+const swagOpts: swaggerJSDoc.Options = {
+    definition: {
+        info: {
+            title: "Auth API",
+            version: "1.0.0",
+            description: "API de autenticación"
+        },
+    },
+    apis: ["./src/index.ts"],
+    authAction: {
+        JWT: {
+            name: "JWT",
+            schema: {
+                type: "apiKey",
+                in: "header",
+                name: "Authorization",
+                description: ""
+            },
+            value: "Bearer <JWT>"
+        }
+    }
+};
+const openAPISpecs = swaggerJSDoc(swagOpts);
+
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
+
+/**
+ * @swagger
+ * /verify:
+ *   post:
+ *     summary: Verifica si el token es válido
+ *     description: Verifica si el token en el header Authorization es válido
+ *     produces:
+ *       - application/json
+ *     responses:
+ *      200:
+ *        description: Token válido
+ *        schema:
+ *        type: json
+ */
 app.post('/verify', expressjwt({
     secret: secret,
     algorithms: ["RS512"]
 }),(_req, _res) => {
-    client.query("SELECT * FROM ", []
-
-    )
+    client.query("SELECT * FROM usuario u WHERE u.id_usuario=$1::text", [])
 });
 
 app.listen(port, () => {
