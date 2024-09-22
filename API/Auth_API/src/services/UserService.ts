b2f385051849f6157e97af5e48deff49168d8e0b
JuanCSUCoder <jcsu2019369@gmail.com>
2024-09-22 18:01:45 -0500

    Fix: Verifications

--- API/Auth_API/src/services/UserService.ts
+++ API/Auth_API/src/services/UserService.ts
@@ -9,7 +9,25 @@ export const UserService = {
     return result.rows as User[];
   },
   findUserByEmail: async (email: string) => {
+    console.log("Buscando usuario con email: " + email);
     const result = await client.query("SELECT * FROM usuario u WHERE u.email = $1", [email]);
+
+    // Verificar si retornó el usuario
+    if (result.rowCount !== null) {
+      console.log("Se encontraron " + result.rowCount + " usuarios");
+
+      if (result.rowCount === 1) {
+        console.log("Usuario encontrado: " + JSON.stringify(result.rows));
+      } else if (result.rowCount > 1) {
+        console.error("Se encontraron múltiples usuarios con el mismo email");
+        return null;
+      } else if (result.rowCount === 0) {
+        console.error("No se encontró ningún usuario con el email: " + email);
+        return null;
+      }
+    }
+
+    console.log("Resultado: " + JSON.stringify(result.rows));
     return result.rows[0] as User;
   },
   authenticate: async (email: string, password: string) => {
