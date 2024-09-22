c64f502ef5c2f02b3e5d86482de58989eba0c563
JuanCSUCoder <jcsu2019369@gmail.com>
2024-09-22 17:33:44 -0500

    Fix: Postgres and add to compose

--- API/Auth_API/src/configs/postgres.ts
+++ API/Auth_API/src/configs/postgres.ts
@@ -4,7 +4,7 @@ const client = new Client({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "password",
     host: process.env.DB_HOST || "host",
-    port: +process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
     database: process.env.DB_DB || "prueba"
 });
 
