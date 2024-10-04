406c131f1dc3fc3c1b52ffab66fd453f042a2d1f
JuanCSUCoder <jcsu2019369@gmail.com>
2024-10-04 17:40:13 -0500

    Feat: Configures /api/auth as the base path for the API

--- API/Auth_API/src/index.ts
+++ API/Auth_API/src/index.ts
@@ -10,11 +10,15 @@ import { generateToken } from "./configs/jwt";
 const app = express()
 const port = process.env.PORT || 2001;
 
-app.use(cors())
-app.use(express.json());
-app.use('/swagger', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
+// Configures /api/auth as the base path for the API
+const router = express.Router();
+app.use('/api/auth', router);
 
-app.post('/login', async (req, res) => {
+router.use(cors())
+router.use(express.json());
+router.use('/swagger', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
+
+router.post('/login', async (req, res) => {
   const login_info = req.body as LoginDTO;
 
   let result = await UserService.authenticate(login_info.email, login_info.contrasena);
@@ -30,7 +34,7 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.put('/asignar_rol', async (req, res) => {
+router.put('/asignar_rol', async (req, res) => {
   const userCategory = req.body as CategoryDTO;
 
   try {
